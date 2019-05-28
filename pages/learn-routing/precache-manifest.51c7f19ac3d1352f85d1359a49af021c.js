a09021851612bf6274fc8dac827b6428d09bf36a
developer-juice <pandalikesart@gmail.com>
2019-05-27 17:27:09 -0700

    added ip routing app and a link to it on the projects page

---
+++ pages/learn-routing/precache-manifest.51c7f19ac3d1352f85d1359a49af021c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd666d2b0e76a4efef8",
+    "url": "/static/css/main.be073558.chunk.css"
+  },
+  {
+    "revision": "1dd666d2b0e76a4efef8",
+    "url": "/static/js/main.ec8f1045.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "47fc8ff8ddfbb5b00aec",
+    "url": "/static/js/2.0ae20bc8.chunk.js"
+  },
+  {
+    "revision": "6aa720274063b2b3e90c306c400d9ed0",
+    "url": "/index.html"
+  }
+];
